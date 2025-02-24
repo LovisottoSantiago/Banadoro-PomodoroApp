4f0cb18040220633bfa4346967cddbedf0bb9b2e
SantiagoLovisotto <santiagolovisotto@gmail.com>
2025-02-23 22:27:53 -0300

    svg

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.banapomodoro {
     requires javafx.fxml;
     requires transitive javafx.graphics;
     requires java.desktop;
-
+    
     opens com.banapomodoro to javafx.fxml;
     exports com.banapomodoro;
 }
